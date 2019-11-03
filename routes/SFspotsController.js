7b97763af1dda7cb14559b6d138e0bff80112d4e
sagdish <forsi1@yahoo.com>
2019-11-03 14:09:49 +0500

    set up

--- routes/SFspotsController.js
+++ routes/SFspotsController.js
@@ -1,7 +1,23 @@
 const router = require('express').Router();
 const axios = require('axios');
+const jwt = require('express-jwt')
+const jwksRsa = require('jwks-rsa');
 
 const Spot = require('../database/SFspotsModel');
+
+// console.log('spot model: ', Spot);
+
+const checkJwt = jwt({
+  secret: jwksRsa.expressJwtSecret({
+    cache: true,
+    rateLimit: true,
+    jwksRequestsPerMinute: 5,
+    jwksUri: `https://sagdi.auth0.com/.well-known/jwks.json`
+  }), 
+  audience: 'ZtCjLTpzf42UPGax1r5PwtptTP8JwMwA',
+  issuer: 'https://sagdi.auth0.com/',
+  algorithms: ['RS256']
+});
 
 router.route('/')
   .get((req, res) => {
@@ -13,8 +29,9 @@ router.route('/')
         res.status(500).json({ error: err });
       });
   })
-  .post((req, res) => {
+  .post(checkJwt, (req, res) => {
     const newSpot = req.body;
+    newSpot.author = req.user.name;
     const spot = new Spot(newSpot);
 
     spot.save()
@@ -29,10 +46,37 @@ router.route('/')
 router.route('/coffeelist')
   .get((req, res) => {
     axios.get(
-      `https://maps.googleapis.com/maps/api/place/nearbysearch/json?location=37.790754,-122.451414&name=&keyword=study,quiet&rankby=distance&key=${process.env.gMapsApi}&type=cafe`
+      // `https://maps.googleapis.com/maps/api/place/nearbysearch/json?location=37.790754,-122.451414&name=&keyword=study,quiet&rankby=distance&key=${process.env.gmapsapi}&type=cafe`
+      `https://maps.googleapis.com/maps/api/place/nearbysearch/json?location=37.790754,-122.451414&name=&keyword=quiet,study&rankby=distance&key=${process.env.gmapsapi}&type=cafe`
+      // `https://maps.googleapis.com/maps/api/place/nearbysearch/json?location=41.3107513,69.287446&name=&keyword=coffee&rankby=distance&key=${process.env.gmapsapi}&type=cafe`
+
     ).then(response => {
       res.json(response.data.results)
     })
   })
 
-  module.exports = router;
+router.route('/libraries')
+  .get((req, res) => {
+    axios.get(
+      `https://maps.googleapis.com/maps/api/place/nearbysearch/json?location=37.790754,-122.451414&name=library&keyword=library&rankby=distance&key=${process.env.gmapsapi}&type=library`
+    ).then(response => {
+      res.json(response.data.results)
+    })
+  })
+
+router.route('/current')
+  .get((req, res) => {
+    const {lat, lng} = req.query
+    console.log(lat, lng);
+    axios.get(
+        // `https://maps.googleapis.com/maps/api/place/nearbysearch/json?location=${lat},${lng}&name=&keyword=study,quiet&rankby=distance&key=${process.env.gmapsapi}&type=cafe`
+        `https://maps.googleapis.com/maps/api/place/nearbysearch/json?location=${lat},${lng}&name=&keyword=coffee&rankby=distance&key=${process.env.gmapsapi}&type=cafe`
+      ).then(response => {
+          // console.log('gmaps response: ', response.data)
+          // res.status(200).json(response.data.results)
+          res.json(response.data.results)
+        })
+        .catch(err => res.status(500).json({ error: err }));
+  })
+
+module.exports = router;
